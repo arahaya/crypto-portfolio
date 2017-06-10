8b2732e0f1cd6abf941bcf2a30b3430971ea96f0
ARAKI Hayato <arahaya.com@gmail.com>
2017-06-11 05:28:59 +0900

    取引所毎のAPI利用からoinmarketcapのAPI利用に変更

--- index.php
+++ index.php
@@ -2,12 +2,7 @@
 
 define('CACHE_DIR', './cache');
 define('CACHE_LIFETIME', 60);
-define('POLONIEX_CURRENCY_API', 'https://poloniex.com/public?command=returnTicker');
-define('BITTREX_CURRENCY_API', 'https://bittrex.com/api/v1.1/public/getmarketsummaries');
-define('BITFLYER_PRICE_API', 'https://bitflyer.jp/api/echo/price');
-define('KRAKEN_ASSETPAIR_API', 'https://api.kraken.com/0/public/AssetPairs');
-define('KRAKEN_CURRENCY_API', 'https://api.kraken.com/0/public/Ticker');
-define('CRYPTOPIA_CURRENCY_API', 'https://www.cryptopia.co.nz/api/GetMarkets/BTC');
+define('COINMARKETCAP_TICKER_API', 'https://api.coinmarketcap.com/v1/ticker/?convert=JPY');
 
 function get_from_cache_or_remote($cache, $remote) {
     $cache_path = CACHE_DIR . '/' . $cache;
@@ -41,68 +36,39 @@ function get_from_cache_or_remote($cache, $remote) {
     return file_get_contents($cache_path);
 }
 
-function fetch_poloniex_currency_market() {
-    return get_from_cache_or_remote('poloniex.json', POLONIEX_CURRENCY_API);
+function fetch_coinmarketcap_ticker_api() {
+    return get_from_cache_or_remote('coinmarketcap.json', COINMARKETCAP_TICKER_API);
 }
 
-function fetch_bittrex_currency_market() {
-    return get_from_cache_or_remote('bittrex.json', BITTREX_CURRENCY_API);
+$data = fetch_coinmarketcap_ticker_api();
+
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
+    echo $data;
+    exit;
 }
-
-function fetch_kraken_currency_market() {
-    $assets =  json_decode(get_from_cache_or_remote('kraken-assets.json', KRAKEN_ASSETPAIR_API), true);
-
-    $pairs = [];
-    foreach ($assets['result'] as $name => $asset) {
-        if ($asset['quote'] === 'XXBT') {
-            $pairs[] = $name;
-        }
-    }
-
-    return get_from_cache_or_remote('kraken.json', KRAKEN_CURRENCY_API . "?pair=" . implode(',', $pairs));
-}
-
-function fetch_cryptopia_currency_market() {
-    return get_from_cache_or_remote('cryptopia.json', CRYPTOPIA_CURRENCY_API);
-}
-
-function fetch_btc_rate() {
-    return get_from_cache_or_remote('btc_rate.json', BITFLYER_PRICE_API);
-}
-
-$poloniex = fetch_poloniex_currency_market();
-$bittrex = fetch_bittrex_currency_market();
-$kraken = fetch_kraken_currency_market();
-$cryptopia = fetch_cryptopia_currency_market();
-$btc_rate = fetch_btc_rate();
-
 ?>
-<!DOCTYPE>
-<html>
+<!doctype html>
+<html lang="ja">
 <head>
-
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>赤字計算機</title>
+    <meta name="description" content="暗号通貨（仮想通貨）の赤字を日本円でわかりやすく管理">
 </head>
 <body>
 
     <app></app>
 
-    <script src="utils.js"></script>
-    <script src="app.html" type="riot/tag"></script>
-    <script src="riot-compiler.min.js"></script>
+    <script src="js/fetch.js"></script>
+    <script src="js/utils.js"></script>
+    <script src="tag/app.html" type="riot/tag"></script>
+    <script src="js/riot-compiler.min.js"></script>
 
     <script>
-        var poloniex = <?= $poloniex ?>;
-        var bittrex = <?= $bittrex ?>;
-        var kraken = <?= $kraken ?>;
-        var cryptopia = <?= $cryptopia ?>;
-        var btc_rate = <?= $btc_rate ?>;
+        var data = <?= $data ?>;
 
         riot.mount('app', {
-            poloniex: poloniex,
-            bittrex: bittrex,
-            kraken: kraken,
-            cryptopia: cryptopia,
-            btc_rate: btc_rate
+            initial_data: data
         });
     </script>
 
